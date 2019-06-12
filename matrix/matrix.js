e93dee8708dac568b83706bd23f33922e58e0bfe
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-06-12 18:37:44 +1000

    early matrix code

---
+++ matrix/matrix.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(grid) {
+    this.grid = grid;
+  }
+
+  get rows() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  get columns() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
